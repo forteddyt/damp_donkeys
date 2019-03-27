8fd13db3cd601bf8eb10279a86e5cbecc4dd14c0
Teddy Todorov <forteddyt@gmail.com>
2019-03-27 15:27:35 -0400

    basic companyList server skeleton

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
+type Company struct {
+	Name string `json:"Name,omitempty"`
+}
+
+var companyList []Company
+
 func main() {
+	log.Print("Starting server...")
+
 	router := mux.NewRouter()
+	
+	router.HandleFunc("/company_list", GetCompanyList).Methods("GET")
+	
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func GetCompanyList(w http.ResponseWriter, r *http.Request){
+	companyList = append(companyList, Company{Name: "Test Name"})
+
+
+	log.Print("Serving companyList")
+	json.NewEncoder(w).Encode(companyList)
 }
