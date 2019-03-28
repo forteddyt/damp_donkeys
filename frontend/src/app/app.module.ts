4c421f4857f9a7c26ba8e692c3a61833ff9967c6
Joe Chu <jchu247@vt.edu>
2019-03-28 18:25:03 -0400

    e	new file:   frontend/src/app/components/registration-complete/registration-complete.component.ts added components for each page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwipeComponent } from './components/swipe/swipe.component';
+import { SelectCompanyComponent } from './components/select-company/select-company.component';
+import { ReviewScreenComponent } from './components/review-screen/review-screen.component';
+import { RegistrationCompleteComponent } from './components/registration-complete/registration-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwipeComponent,
+    SelectCompanyComponent,
+    ReviewScreenComponent,
+    RegistrationCompleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/review-screen/review-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-screen',
+  templateUrl: './review-screen.component.html',
+  styleUrls: ['./review-screen.component.css']
+})
+export class ReviewScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
