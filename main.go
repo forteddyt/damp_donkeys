b27082dcbb8437d4771e313495e17862d422be30
Teddy Todorov <forteddyt@gmail.com>
2019-03-28 15:45:29 -0400

    skeleton code of post req

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	// "exec" // For running perl script to get info from 90 number
 )
 
+// Company type information
 type Company struct {
 	Name string `json:"Name,omitempty"`
 }
@@ -19,6 +21,7 @@ func main() {
 	router := mux.NewRouter()
 	
 	router.HandleFunc("/company_list", GetCompanyList).Methods("GET")
+	router.HandleFunc("/put_student", PutStudent).Methods("POST")
 	
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -29,4 +32,12 @@ func GetCompanyList(w http.ResponseWriter, r *http.Request){
 
 	log.Print("Serving companyList")
 	json.NewEncoder(w).Encode(companyList)
+}
+
+func PutStudent(w http.ResponseWriter, r *http.Request){
+	params := r.URL.Query()
+
+	log.Print(params["VT_ID"])
+
+	// exec.Command(/* shell script */, /* 90-number */)
 }
