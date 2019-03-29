ba3120d9c425de43290bf2ac9fec3e75c56fe4ab
Teddy Todorov <forteddyt@gmail.com>
2019-03-29 10:26:56 -0400

    started work on perl script replication

---
+++ edidutil/edid.go
@@ -0,0 +1,12 @@
+package edid
+
+import (
+	"github.com/emersion/go-sasl"
+	"gopkg.in/ldap.v3"
+)
+
+func DataFor(){
+	my_sasl := sasl.NewExternalClient("dn:uusid=cs-career-fair,ou=services,dc=vt,dc=edu")
+
+	
+}
