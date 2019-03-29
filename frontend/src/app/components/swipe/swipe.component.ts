6681d3e1753f890384d05ab4f1f13cb0afa63b24
Travis Rieger <trieg@vt.edu>
2019-03-28 21:30:52 -0400

    PID input, without API calls

--- frontend/src/app/components/swipe/swipe.component.ts
+++ frontend/src/app/components/swipe/swipe.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swipe.component.css']
 })
 export class SwipeComponent implements OnInit {
-
+  title = "Enter PID"
+  PIDInput = "Enter your PID"
+  pid = ''
   constructor() { }
 
   ngOnInit() {
+    document.getElementById("pid_input").focus();
   }
 
+  onKey(event: any) { // without type info
+    var input = event.target.value;
+    if (input.length < 9) {
+      this.pid = event.target.value;
+    }
+    else {
+      var start = input.indexOf('9');
+      if (start != -1) {
+        this.pid = input.substring(start, start + 9);
+      } 
+    }
+    if (this.pid.length == 9 && /^\d+$/.test(this.pid)) {
+      //API Call here and move onto next page
+    }
+    event.target.value = this.pid;
+  }
 }
