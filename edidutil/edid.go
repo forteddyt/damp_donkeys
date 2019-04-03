c82d499ff2ee700e90c767ea793b678ebe7272c1
forteddyt <ttodorov@vt.edu>
2019-04-03 07:46:36 -0400

    edidutil returns mapping of info

--- edidutil/edid.go
+++ edidutil/edid.go
@@ -4,13 +4,23 @@ import (
     "fmt"
     "log"
     "os/exec" // For running perl script to get info from 90 number
+    "strings"
 )
 
-func ObtainEdidInfo(uid string) {
+func ObtainEdidInfo(uid string) map[string] string{
+    student := map[string]string{"dispName": "", "major": "", "class": ""}
     out, err := exec.Command("/usr/bin/perl", "edidutil/edid.pl", uid).Output()
     if err != nil {
         log.Printf("ERROR: [%s]\n", err)
+	return student
     } else {
-        fmt.Printf("%s\n", out)
+        fmt.Printf("Requested edid.pl, got [%s]\n", out)
+	s := strings.Split(string(out), ";") //convert []byte to a string and split on ';'
+	
+	student["dispName"] = s[0]
+	student["major"] = s[1]
+	student["class"] = s[2]
+	
+	return student
     }
 }
