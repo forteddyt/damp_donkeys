d072a8fd2a0077f1e08666c3e9f03ff7a89030da
Teddy Todorov <ttodorov@vt.edu>
2019-04-08 22:51:56 -0400

    added Login api endpoint

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"os"
+	"io/ioutil"
 	"log"
+	"errors"
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"github.com/damp_donkeys/edidutil"
@@ -11,21 +14,18 @@ import (
 	"github.com/damp_donkeys/dbutil"
 )
 
-// Company type information
-type Company struct {
-	Name string `json:"Name,omitempty"`
-}
+const(
+	JWT_DURATION = 30 // In minutes. How long the token will stay valid between requests
+)
 
-type Student struct{
-	DisplayName string `json:"DisplayName,omitempty"`
-	Class string `json:"Class,omitempty"` // Freshman, Sophomore, Junior, Senior
-	Major string `json:"Major,omitempty"`
-}
-
-var companyList []Company
+// Credentials obtained from .gitignored files on server startup
+var DBPassword, DBUsername string
 
 func main() {
 	log.Print("Starting server...")
+
+	err := credentialSetup()
+	if err != nil { log.Fatal("Credential setup failed!") }
 
 	router := mux.NewRouter()
 	
@@ -41,16 +41,41 @@ func main() {
 	})
 	
 	handler := cors.Default().Handler(router)
-	// handler = c.Handler(handler)
+	handler = c.Handler(handler)
 	http.ListenAndServe(":8080", handler)
 }
 
+func credentialSetup() error{
+	gopath := os.Getenv("GOPATH")
+	if len(gopath) == 0 {
+		return errors.New("GOPATH environment variable not set; Cannot obtain credentials")
+	}
+
+	log.Println("Opening server's secret.json...")
+	jsonFile, err := os.Open(gopath + "/src/github.com/damp_donkeys/server/secret.json")
+
+	if err != nil { return err }
+	defer jsonFile.Close() // Close json file at end
+	defer log.Println("Closing server's secret.json") // Then log the close
+
+	byteValue , _ := ioutil.ReadAll(jsonFile) // Read json as []byte
+	var result map[string]map[string]string // result will be a mapping of string to a mapping of string to string
+
+	json.Unmarshal(byteValue, &result)
+
+	log.Println("Getting database username...")
+	DBUsername = result["database"]["username"]
+	log.Println("Database username obtained!")
+	log.Println("Getting database password...")
+	DBPassword = result["database"]["password"]
+	log.Println("Database password obtained!")
+
+	return nil
+}
+
 func GetCompanyList(w http.ResponseWriter, r *http.Request){
-	companyList = append(companyList, Company{Name: "Test Name"})
-
-
 	log.Print("Serving companyList")
-	json.NewEncoder(w).Encode(companyList)
+	json.NewEncoder(w).Encode(map[string]string{})
 }
 
 func GetStudent(w http.ResponseWriter, r *http.Request){
@@ -71,24 +96,31 @@ func GetStudent(w http.ResponseWriter, r *http.Request){
 }
 
 func Login(w http.ResponseWriter, r *http.Request){
+	params := r.URL.Query()
 	log.Printf("login api called with [%s]\n", params)
 
-	params := r.URL.Query()
-	givenPwdHash := params["password_hash"]
+	givenPwdHash := params["password_hash"][0]
 
 	// Client request error
-	if(givenPwdHash == nil){
+	if(givenPwdHash == ""){
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode()
+		json.NewEncoder(w).Encode(map[string]string{})
 	} else {
-		dbconn, err := dbutil.OpenDB("")
-		user, err = "", nil // dbutil.getUser(givenPwdHash)
+		dbconn, err := dbutil.OpenDB("dev", DBUsername, DBPassword)
+		if err != nil {
+			log.Printf("Database connection failed: %s\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{})
+			return
+		}
+		defer dbutil.CloseDB(dbconn)
+		user, err := dbutil.CheckPasswordHash(dbconn, givenPwdHash) // dbutil.getUser(givenPwdHash)
 
 		// Database request error
 		if err != nil {
-			log.Printf("Could not get password hash from database\n")
+			log.Printf("Could not get user from database: %s\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode()
+			json.NewEncoder(w).Encode(map[string]string{})
 			return
 		}
 
@@ -96,9 +128,22 @@ func Login(w http.ResponseWriter, r *http.Request){
 		if user == "" {
 			log.Printf("No user with password hash '%s' found\n", givenPwdHash)
 			w.WriteHeader(http.StatusUnauthorized)
-			json.NewEncoder(w).Encode()
+			json.NewEncoder(w).Encode(map[string]string{})
+			return
 		}
 
+		jwt, err := jwtutil.CreateToken(user, JWT_DURATION)
+		if err != nil {
+			log.Printf("JWT Token creation failed: %s\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{})
+			return
+		}
+
+		m := map[string]string{"jwt": jwt}
+		log.Printf("JWT Token created successfully, valid for %s minutes\n", JWT_DURATION)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(m)
 	}
 }
 
