e7d6b843a6dc713ce273e0c3e786f4d5dd884fcf
Teddy Todorov <forteddyt@gmail.com>
2019-04-19 15:26:50 -0400

    implemented admin login verification

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as jwt_decode from 'jwt-decode';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+	code = ''
+	constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
-  constructor() { }
+	ngOnInit() {
+		document.getElementById("admin_code").focus();
+	}
 
-  ngOnInit() {
-  }
+	onKey(event: any)
+	{
+		this.code = event.target.value;
+	}
 
+	handleClick(event: any)
+	{
+		this.http.get("https://csrcint.cs.vt.edu/api/login?code=" + this.code, {observe: 'response'}).subscribe(
+		    resp => {
+		    	var jwt = resp.body["jwt"]
+				var decoded = jwt_decode(jwt);
+
+				// Only the user "admin" should be able to log in
+				if(decoded["user"] == "admin"){
+			    	this.router.navigateByUrl('/admin/nav');
+				} else {
+					alert("Invalid Code")					
+				}
+
+		    },
+		    error => {
+				alert("Invalid Code")
+				console.log(error)
+		    }
+		  );
+	}
 }
