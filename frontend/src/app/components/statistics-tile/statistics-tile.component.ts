56abe692ab81b7f1e4a3598a73722ea450ccd550
Teddy Todorov <forteddyt@gmail.com>
2019-04-20 18:22:46 -0400

    auto-update statistics

--- frontend/src/app/components/statistics-tile/statistics-tile.component.ts
+++ frontend/src/app/components/statistics-tile/statistics-tile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
 	selector: 'app-statistics-tile',
@@ -7,6 +9,8 @@ import { HttpClient } from '@angular/common/http';
 	styleUrls: ['./statistics-tile.component.css']
 })
 export class StatisticsTileComponent implements OnInit {
+	sub: Subscription;
+
 	stateData
 	careerFairName
 	companies
@@ -15,21 +19,32 @@ export class StatisticsTileComponent implements OnInit {
 
 	constructor(private http: HttpClient) {}
 
-	async ngOnInit() {
+	ngOnInit() {
+		const source = interval(60 * 1000);
+		this.updateStats();
+		this.sub = source.subscribe((val) => { this.updateStats() })
+	}
+
+	ngOnDestroy(){
+		this.sub && this.sub.unsubscribe();
+	}
+
+	getStats() {
+		return this.http.get("https://csrcint.cs.vt.edu/api/career_fair_stats?career_fair_name=" + this.careerFairName + "&jwt=" + this.stateData.jwt, {observe: 'response'}).toPromise();
+	}
+
+	async updateStats() {
+		console.log("updating...")
 		let resp = await this.getStats()
 		if (resp.status != 200) {
-			this.companies = "!"
-			this.numStudents = "!"
-			this.numInterviews = "!"
+			this.companies = "error!"
+			this.numStudents = "error!"
+			this.numInterviews = "error!"
 			return
 		}
 
 		this.companies = resp.body["company_list"]
 		this.numStudents = resp.body["student_count"]
 		this.numInterviews = resp.body["interview_count"]
-	}
-
-	getStats() {
-		return this.http.get("https://csrcint.cs.vt.edu/api/career_fair_stats?career_fair_name=" + this.careerFairName + "&jwt=" + this.stateData.jwt, {observe: 'response'}).toPromise();
 	}
 }
